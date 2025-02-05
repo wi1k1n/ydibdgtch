fbd7e2da2508ec93de3158d8dc949f47a2194930
Ilya Mazlov <mazlov.i.a@gmail.com>
2025-02-05 17:16:24 +0100

    formatting

--- source/win/main.cpp
+++ source/win/main.cpp
@@ -90,50 +90,50 @@ int runEngineWeb(int argc, char* argv[]) {
 }
 
 int main(int argc, char* argv[]) {
-    std::cout << "|-- ydibdgtch --|" << std::endl;
+  std::cout << "|-- ydibdgtch --|" << std::endl;
 
-		if (argc < 2) {
-			std::cout << "Usage: " << argv[0] << " uci|engineweb|tests" << std::endl;
-			return 1;
-		}
+	if (argc < 2) {
+		std::cout << "Usage: " << argv[0] << " uci|engineweb|tests" << std::endl;
+		return 1;
+	}
 
-		std::string command = argv[1];
-		if (command == "uci") {
-			std::cout << "UCI mode isn't yet implemented" << std::endl;
-			return 0;
-		}
+	std::string command = argv[1];
+	if (command == "uci") {
+		std::cout << "UCI mode isn't yet implemented" << std::endl;
+		return 0;
+	}
 
-		if (command == "engineweb") {
-			return runEngineWeb(argc, argv);
-		}
+	if (command == "engineweb") {
+		return runEngineWeb(argc, argv);
+	}
 
-		if (command == "tests") {
-			std::cout << "Tests mode isn't yet implemented" << std::endl;
-			return 0;
-		}
+	if (command == "tests") {
+		std::cout << "Tests mode isn't yet implemented" << std::endl;
+		return 0;
+	}
 
 
 
-		//ChessGameState gameState;
-		//std::cout << gameState.ToString() << std::endl << std::endl;
-		//std::cout << gameState.ToString(false) << std::endl << std::endl;
-		//std::cout << gameState.ToString(true, false) << std::endl << std::endl;
-		//std::cout << gameState.ToString(true, true, true) << std::endl << std::endl;
+	//ChessGameState gameState;
+	//std::cout << gameState.ToString() << std::endl << std::endl;
+	//std::cout << gameState.ToString(false) << std::endl << std::endl;
+	//std::cout << gameState.ToString(true, false) << std::endl << std::endl;
+	//std::cout << gameState.ToString(true, true, true) << std::endl << std::endl;
 
-		//ChessGameState gameState2(String("rn1qkbnr/pp2pppp/2p5/5b2/3PN3/8/PPP2PPP/R1BQKBNR w KQkq - 0 1"));
-		//std::cout << gameState2.ToString() << std::endl << std::endl;
-		//gameState2.Set(1, 4, ChessPiece('Q'));
-		//std::cout << gameState2.ToFEN() << std::endl << std::endl;
+	//ChessGameState gameState2(String("rn1qkbnr/pp2pppp/2p5/5b2/3PN3/8/PPP2PPP/R1BQKBNR w KQkq - 0 1"));
+	//std::cout << gameState2.ToString() << std::endl << std::endl;
+	//gameState2.Set(1, 4, ChessPiece('Q'));
+	//std::cout << gameState2.ToFEN() << std::endl << std::endl;
 
-		//std::cout << "========================================" << std::endl;
+	//std::cout << "========================================" << std::endl;
 
-		//ClassicChessRules rules;
-		//ChessGameState state(String("r1bqk2r/pppp1ppp/2nb1n2/1B2p3/4P3/3P1N2/PPP2PPP/RNBQK2R w KQkq - 0 1"));
-		//std::cout << state.ToString() << std::endl << std::endl;
-		//state.MakeMove(ChessStateMove(ChessPieceLocation(0, 4), ChessMoveLocation(0, 6, CHESSMOVEINFO::CASTLING)));
-		//std::cout << state.ToString() << std::endl << std::endl;
+	//ClassicChessRules rules;
+	//ChessGameState state(String("r1bqk2r/pppp1ppp/2nb1n2/1B2p3/4P3/3P1N2/PPP2PPP/RNBQK2R w KQkq - 0 1"));
+	//std::cout << state.ToString() << std::endl << std::endl;
+	//state.MakeMove(ChessStateMove(ChessPieceLocation(0, 4), ChessMoveLocation(0, 6, CHESSMOVEINFO::CASTLING)));
+	//std::cout << state.ToString() << std::endl << std::endl;
     
-    return 0;
+  return 0;
 }
 
 std::vector<std::string> split(const std::string& s, char seperator) {
